26e26360930023fdee89c50a4a45d00ae0954476
rainrun <33125415+rainrun@users.noreply.github.com>
2018-07-06 14:35:38 +0800

    增加sql配置

---
+++ src/main/resources/sql/dashboard.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS `dashboard` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `tenant_id` INT,
+    `customer_id` INT,
+	`name` varchar(31),
+	PRIMARY KEY(`id`),
+	KEY `relation_tenantId` (`tenant_id`),
+	KEY `relation_customerId` (`customer_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `dashboard`
+ADD CONSTRAINT `dashboard_tenantId` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`id`);
+
+ALTER TABLE `dashboard`
+ADD CONSTRAINT `dashboard_customerId` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+CREATE TABLE IF NOT EXISTS `widget_entity` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `dashboard_id` INT,
+	`device_id` varchar(31),
+	`name` varchar(31),
+	`entity_type` varchar(31),
+	`startTime` bigint,
+	`endTime` bigint,
+	PRIMARY KEY(`id`),
+	KEY `relation_dashboardId` (`dashboard_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `widget_entity`
+ADD CONSTRAINT `widget_entity_dashboardId` FOREIGN KEY (`dashboard_id`) REFERENCES `dashboard` (`id`);
