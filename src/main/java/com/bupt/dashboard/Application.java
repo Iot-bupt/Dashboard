3ef5892911344c84ee5d83c07a178d2436f3da28
rainrun <33125415+rainrun@users.noreply.github.com>
2018-07-10 21:13:51 +0800

    修改配置disconf

--- src/main/java/com/bupt/dashboard/Application.java
+++ src/main/java/com/bupt/dashboard/Application.java
@@ -1,11 +1,18 @@
 package com.bupt.dashboard;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@EnableCaching
+@ComponentScan("com.bupt.dashboard")
+@MapperScan("com.bupt.dashboard.mapper")
+@PropertySource({"classpath:disconf.properties"})
+@ImportResource({"classpath:disconf.xml"})//引入disconf
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
